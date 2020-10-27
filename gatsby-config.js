d64dd17a812750db651fc42aa0786dfdca744798
Alan M <alan.maldonado.lara1@gmail.com>
2020-10-26 21:18:07 -0600

    Basic layout, post template and Gatbsy configuration

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+const siteMetadata = {
+  title: `The Localhost Blog`,
+  description: `This is my coding blog where I write about my coding journey.`,
+};
+
+module.exports = {
+  siteMetadata: siteMetadata,
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`,
+      },
+    },
+  ],
+};
---
+++ gatsby-node.js
@@ -0,0 +1,51 @@
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require('path');
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+  const postTemplate = path.resolve('src/templates/BlogPost.js');
+
+
+  return graphql(`
+    {
+      allMdx {
+        nodes {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if(result.errors) {
+      throw result.errors
+    }
+
+    const posts = result.data.allMdx.nodes;
+
+    posts.forEach(post => {
+      createPage({
+        path: post.fields.slug,
+        component: postTemplate,
+        context: {
+          slug: post.fields.slug,
+        },
+      })
+    })
+  });
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `Mdx`) {
+    const value = createFilePath({ node, getNode });
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    });
+  }
+};
